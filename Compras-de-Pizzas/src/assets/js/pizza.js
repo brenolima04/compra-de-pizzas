15c6b604dc2ccc4bdc8dc0483d99a40234dcc192
brenolima04 <brenonlima04@gmail.com>
2021-10-24 12:25:21 -0300

    Finalizado minha página e atualizado o README

---
+++ Compras-de-Pizzas/src/assets/js/pizza.js
@@ -0,0 +1,14 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'./src/assets/images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'./src/assets/images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'./src/assets/images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'./src/assets/images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Frango', img:'./src/assets/images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'./src/assets/images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'./src/assets/images/pizza.png', price:28.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:8, name:'Portuguesa', img:'./src/assets/images/pizza2.png', price:32.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:9, name:'Pizza da Casa', img:'./src/assets/images/pizza3.png', price:48.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:10, name:'Frangoesa', img:'./src/assets/images/pizza4.png', price:19.35, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:11, name:'Polonesa', img:'./src/assets/images/pizza5.png', price:85.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:12, name:'Amori', img:'./src/assets/images/pizza6.png', price:26.06, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+];
---
+++ README.md
@@ -0,0 +1,66 @@
+<h1 align="center">
+    <div href="#">🔗 Compra de Pizzas</div>
+</h1>
+
+<h3 align="center">🚀 Descrição:</h3>
+
+<div align="center">
+    Página feita utilizando algumas tecnologias bácias para web: HTML5, CSS3, JavaScript, Sass.
+    Nela podemos adicionar vários tipode  pizzas, e colocar dentro de um carrinho, e nele
+    podemos ver o tamanho da piza, valor, descontos e outras funções.
+</div>
+
+<hr>
+
+<div>
+	<h3 style="font-weight: normal;" align="center">🛠 Tecnologias:</h3>
+</div>
+
+
+<div align="center">
+    <img style="margin: 10px;" src="https://img.shields.io/badge/HTML5-tecnology-yellow" alt="">
+    <img style="margin: 10px;" src="https://img.shields.io/badge/JavaScript-tecnology-yellowgreen" alt="">
+    <img style="margin: 10px;" src="https://img.shields.io/badge/CSS3-tecnology-orange" alt="">
+    <img style="margin: 10px;" src="https://img.shields.io/badge/sass-tecnology-lightgrey" alt="">
+</div>
+
+<hr>
+
+<h3 style="font-weight: normal;" align="center">🚀 Como usar:</h3>
+<div align="center"">
+  Como é uma página em html normal, basta apenas baixar o projeto e dar duplo clique no arquivo
+  que tem o nome: index.html
+
+<hr>
+	    
+</div>
+<h3 align="center"> 
+	🚧  Status do Projeto:  Finalizado!  🚧
+</h3>
+<hr>
+
+<h3 align="center"> 🚀 Demostração da página</h3>
+
+<div align="center">
+    <img src="./Compras-de-Pizzas/src/assets/images/pizza.gif"  width="auto" height="auto" alt="">
+</div>
+										     
+<hr>
+										     
+<h3 align="center">🚀 Agradecimentos:</h3>
+		  
+<div>
+Obrigado por está interessado nos meus sistemas, espero que de alguma forma eu possa
+ajudar você nos seus estudos, e que possa aproveitar algo de bom aqui! <br><br>
+Meu linkedin: Breno Nicácio <br><br>	   
+</div>		  
+
+	   
+<hr>      
+
+
+MIT License
+@Copyright (c) 2021 Breno Nicácio
+
+
+
